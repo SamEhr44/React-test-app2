84f81bf730e0504624505b89ffb271ee7df666ab
SamEhr44 <106220127+SamEhr44@users.noreply.github.com>
2023-04-20 14:41:23 -0600

    first

--- server/server.js
+++ server/server.js
@@ -1,19 +1,12 @@
 const path = require('path');
 const express = require('express');
-const publicPath = path.join(__dirname, '..', 'client/public/index.html');
-
-const app = express(),
-      bodyParser = require("body-parser");
-      port = process.env.PORT || 3080;
-app.timeout = 0;
-app.use(express.static(path.join(__dirname, '../client/build')));
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-   res.sendFile(publicPath);
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.get('/', (req,res) => {
-   res.send(`<h1>API Running on the port ${port}</h1>`);
- });
 app.listen(port, () => {
-   console.log('Server is up! ${port}');
-   console.log(port);
+   console.log('Server is up!');
 });
